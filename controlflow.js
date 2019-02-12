3a6c5e8d1a846401e00d7649ff24d72c48e36dbd
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-02-12 14:41:25 +0000

    Update controlflow.js

--- controlflow.js
+++ controlflow.js
@@ -14,8 +14,11 @@ if(early && age >18){
   console.log(`Your Race Number is ${raceNumber} and your race will begin at 1100`);
 } else if(age <18){
   console.log(`Your Race Number is ${raceNumber} and your race will begin at 1230`);
-} else{
+} else if(age === 18) {
   console.log('Please report to the registration desk as you are on the cutoff for adult racing!');
 }
+  else {
+    console.log('Hmmm, seems something is amiss here. Come to the help desk!');
+  }
 
 //control flow block sorts people into the right race - first early adults, then late adults, then kids then if they are 18 they come to the desk to register.
